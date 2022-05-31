e94454e1da5bc1e8b3afba5a38d8ac49709e82f7
Lukáš Punt <lukas.punt@educhem.cz>
2022-05-31 15:22:07 +0200

    update 1.0.1

--- KamenNozniceApapirus/Program.cs
+++ KamenNozniceApapirus/Program.cs
@@ -1,4 +1,5 @@
-﻿using Spectre.Console;
+﻿global using KamenNozniceApapirus;
+using Spectre.Console;
 
 Random generator = new Random();
 
@@ -57,24 +58,104 @@ if (odpoved == "EXIT")
     Console.Clear();
     Environment.Exit(1);
 }
+Score idk = new Score();
 
 Console.Clear();
 #region 
 Console.ForegroundColor = ConsoleColor.Blue;
-Console.WriteLine("You: 0");
+Console.WriteLine($"You: {idk.TvojeScore}");
 Console.ForegroundColor = ConsoleColor.DarkRed;
-Console.WriteLine("Enemy: 0");
+Console.WriteLine($"Enemy: {idk.EnemyScore = 0}");
 Console.WriteLine("--------------------");
 Console.WriteLine("Kolo hráče (tebe):");
 Console.WriteLine("    1 - kámen");
 Console.WriteLine("    2 - nůžky");
 Console.WriteLine("    3 - papír");
 Console.WriteLine("--------------------");
-
+#endregion
 string hrac = Console.ReadLine();
-if (hrac == "1")
+
+
+int volba = generator.Next(1, 4);
+
+
+if (volba == 1)
 {
+    if (hrac == "1")
+    {
+        Console.WriteLine("nepřítel zvolil kámen");
+        Console.WriteLine(" je to remíza ");
+    }
+    else if (hrac == "3")
+    {
+        Console.WriteLine("nepřítel zvolil papír");
+        Console.WriteLine(" je to remíza ");
+
+    }
+    else if (hrac == "2")
+    {
+        Console.WriteLine("nepřítel zvolil nůžky");
+        Console.WriteLine("je to remíza ");
+    }
+    else
+    {
+        Console.WriteLine("musíš si vybrat kámen, nůžky nebo papír!");
+
+    }
 
 }
 
-int volba = generator.Next(1, 4);
+else if (volba == 2)
+{
+    if (hrac == "1")
+    {
+        Console.WriteLine("nepřítel zvolil papír");
+        Console.WriteLine("prohrál si");
+        idk.EnemyScore ++;
+
+    }
+    else if (hrac == "3")
+    {
+        Console.WriteLine("nepřítel zvolil nůžky");
+        Console.WriteLine("prohrál si ");
+        idk.EnemyScore++;
+
+    }
+    else if (hrac == "2")
+    {
+        Console.WriteLine("nepřítel zvolil kámen");
+        Console.WriteLine("prohrál si");
+        idk.EnemyScore++;
+    }
+    else
+    {
+        Console.WriteLine("musíš si vybrat kámen, nůžky nebo papír!");
+    }
+}
+else if (volba == 3)
+{
+    if (hrac == "1")
+    {
+        Console.WriteLine("nepřítel zvolil nůžky");
+        Console.WriteLine("vyhrál si!");
+        idk.TvojeScore ++;
+
+    }
+    else if (hrac == "3")
+    {
+        Console.WriteLine("nepřítel zvolil kámen");
+        Console.WriteLine("vyhrál si");
+        idk.TvojeScore++;
+    }
+    else if (hrac == "2")
+    {
+        Console.WriteLine("nepřítel zvolil papír");
+        Console.WriteLine("vyhrál si");
+        idk.TvojeScore++;
+    }
+    else
+    {
+        Console.WriteLine("musíš si vybrat kámen, nůžky nebo papír!");
+        idk.TvojeScore++;
+    }
+}
---
+++ KamenNozniceApapirus/Score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KamenNozniceApapirus
+{
+    internal class Score
+    {
+        public int TvojeScore { get; set; }
+        public int EnemyScore { get; set; }
+        public void lol()
+        {
+            TvojeScore = 0;
+            EnemyScore = 0;
+        }
+    }
+    
+}

4a27fa502f4f3d4aa65975513ba3893a98753fad
Lukáš Punt <lukas.punt@educhem.cz>
2022-06-06 19:05:12 +0200

    Update Shopu Beta

--- KamenNozniceApapirus/Score.cs
+++ KamenNozniceApapirus/Score.cs
@@ -9,12 +9,16 @@ namespace KamenNozniceApapirus
     internal class Score
     {
         public int TvojeScore { get; set; }
+        public int celkoveTvojeScore { get; set; }
         public int EnemyScore { get; set; }
        public bool playAgain { get; set; }
+        public bool restart { get; set; }
         public void lol()
         {
             TvojeScore = 0;
             EnemyScore = 0;
+            celkoveTvojeScore = 0;
+            
         }
     }
     
